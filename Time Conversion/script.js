a95cb77c46a3f9d37e858543518dee3abd4a2bd5
SouzaEdgar <edgarss2013@gmail.com>
2023-08-24 02:42:21 -0300

    resolvido

--- Time Conversion/script.js
+++ Time Conversion/script.js
@@ -1,4 +1,4 @@
-var input = require('fs').readFileSync('./Mini-Max Sum/stdin', 'utf8');
+var input = require('fs').readFileSync('./Time Conversion/stdin', 'utf8');
 var lines = input.split('\n');
 
 /*
@@ -9,4 +9,24 @@ var lines = input.split('\n');
     return '00:01:00'
 */
 
+function timeConversion(s) {
+    let hr = s.substr(0,2);
 
+    if(s.endsWith('PM')) {
+        s = s.replace('PM','');
+        if(parseInt(hr) < 12) {
+           s = s.replace(`${hr}`,`${parseInt(hr) + 12}`);
+        }
+    } else {
+        s = s.replace('AM','');
+        if(parseInt(hr) >= 12) {
+           s = s.replace(`${hr}`, `${parseInt(hr) - 12}`);
+           s = 0 + s;
+        }
+    }
+    return s;
+}
+
+let s = input;
+const result = timeConversion(s);
+console.log(result + '\n');
