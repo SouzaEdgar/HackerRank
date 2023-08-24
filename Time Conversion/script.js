cc41190dd97448db9ff83f6d5cfbfd7bbe503d95
SouzaEdgar <edgarss2013@gmail.com>
2023-08-24 02:09:24 -0300

    novo problema

---
+++ Time Conversion/script.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('./Mini-Max Sum/stdin', 'utf8');
+var lines = input.split('\n');
+
+/*
+    s = '12:01:00PM'
+    return '12:01:00'
+
+    s = '12:01:00AM'
+    return '00:01:00'
+*/
+
+
